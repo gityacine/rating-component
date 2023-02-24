0631cdffc25d2d9ed9eca7e063985a7df7b675a7
Yacine Haddouche <100038941+gityacine@users.noreply.github.com>
2023-02-24 16:52:34 +0100

    made the design responsive, and added some visual enhancements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Card from "./components/Card";
 
 function App() {
-  
+
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#121417]">
+    <div className="flex min-h-screen items-center justify-center bg-[#121417] p-6">
       <Card />
     </div>
-    
+
   )
 }
 
--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 
 export default function Circle(props) {
 
-    const [isSelected, setIsSelected] = useState(false)
-
     return (
-        <div onClick={() => { props.selection(props.number) }} className={`flex items-center justify-center w-12 h-12 ${props.chosen === props.number ? 'bg-[#7c8798]' : 'bg-[#343f4c]'} rounded-full`}>
-            <span className={`${props.chosen === props.number ? 'text-white' : 'text-[#7c8798]'} font-overpass`}>{props.number}</span>
-        </div>
+        <button onClick={() => { props.selection(props.number) }} className={`flex items-center justify-center shadow-lg w-12 h-12 ${props.chosen === props.number ? 'bg-[#7c8798]' : 'bg-[#343f4c]'} rounded-full ${props.chosen === props.number ? 'text-white' : 'text-[#7c8798]'} text-lg font-normal font-overpass transition ease-in-out sm:hover:scale-110 duration-500 sm:hover:bg-[#fb7413] sm:hover:text-white active:bg-[#fb7413] active:text-white`}>
+            {props.number}
+        </button>
     );
 }
